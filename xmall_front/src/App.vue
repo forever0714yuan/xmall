123ba70ae0a46dc50b999c0dddfc83b87840b877
于金元 <yujinyuan@sefonsoft.com>
2023-02-02 16:58:25 +0800

    jenkins

--- xmall_front/src/App.vue
+++ xmall_front/src/App.vue
@@ -4,14 +4,15 @@
   </div>
 </template>
 
-<style lang='scss'>
-  @import 'assets/style/index.scss';
-  #app{
-    height: 100%;
-  }
-  .main{
-    background-color: #ededed;
-    overflow: hidden;
-    width: 100%;
-  }
+<style lang="scss">
+//搜索
+@import 'assets/style/index.scss';
+#app {
+  height: 100%;
+}
+.main {
+  background-color: #ededed;
+  overflow: hidden;
+  width: 100%;
+}
 </style>
